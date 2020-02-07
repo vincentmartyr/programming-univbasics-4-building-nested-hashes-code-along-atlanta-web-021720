e44260f2d3096af5428ab7b9980637c36d4ff5db
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 19:13:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,43 +1,17 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-        #your key/value pairs here
-        },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-        #your key/value pairs here
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        #your key/value pairs here
-        },
+      :patriarch => {:name => "Lord Montague",:age => "53"},
+      :matriarch => {:name => "Lady Montague",:age => "54"},
+      :hero => {:name => "Romeo",:age => "15",:status => "alive"},
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-        #your key/value pairs here
-        },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-        #your key/value pairs here
-        },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-        #your key/value pairs here
-        },
-      :heroine_friends=>[]
-   }
+      :patriarch => {:name => "Lord Capulet",:age => "50"},
+      :matriarch => {:name => "Lady Capulet",:age => "51"},
+      :heroine => {:name => "Juliet",:age => "15",:status => "alive"},
+      :heroine_friends => []
+
  }
 }
 end
