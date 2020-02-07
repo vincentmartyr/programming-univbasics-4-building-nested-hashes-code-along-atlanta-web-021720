569d7a07f4d3716ea6468549075536b0f479c785
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 19:21:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,6 @@ def third_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status:"alive"},
       :heroine_friends => []
-
- }
-
+   }
+}
 end
